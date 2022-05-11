722ec90e5da330c9838f2f0608ed994a24e12304
cho seonghoon <chsida655@gmail.com>
2022-05-11 23:30:34 +0900

    coding test solved

---
+++ 백준/bj1018.py
@@ -0,0 +1,11 @@
+n, m = map(int,input().split())
+
+board=[]
+for _ in range(n):
+    row = list(input())
+    board.append(row)
+
+case = [[0 for _ in range()]]
+
+for i in range(n-7):
+    for j in range(m-7):
